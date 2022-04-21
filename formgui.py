41a446cf16806fc9b27d858b8b408be886efc36e
Vasiliy Alchakov <v.alchakov@gmail.com>
2022-04-21 22:42:22 +0300

    New implementation for test scripts

--- formgui.py
+++ formgui.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Global Varibales
 title_window = 'HSV Tuner'
 lowh = 0
-lows = 21
-lowv = 12
+lows = 50
+lowv = 0
+
 hih = 10
 his = 255
 hiv = 255
@@ -12,7 +15,7 @@ hiv = 255
 
 def draw_contour(img_org, img_thresh):
     contours, hierarchy = cv2.findContours(image=img_thresh,
-                                           mode=cv2.RETR_TREE,
+                                           mode=cv2.RETR_EXTERNAL,
                                            method=cv2.CHAIN_APPROX_NONE)
     # Draw contours on the original image
     img_contour = img_org.copy()
@@ -22,19 +25,36 @@ def draw_contour(img_org, img_thresh):
                      color=(0, 255, 0),
                      thickness=2,
                      lineType=cv2.LINE_AA)
+    img_contour = cv2.resize(img_contour, (1280, 800))
+    cv2.namedWindow('Contour image')
+    cv2.moveWindow('Contour image', 10 + 640 + 10, 10)
     cv2.imshow('Contour image', img_contour)
 
 
-def call_inrrange():
-    global img_hsv
-    global img_original
-    global lowh
-    global lows
-    global lowv
-    global hih
-    global his
-    global hiv
+def draw_histogram(img_org, img_cor):
+    plt.figure(figsize=(8, 5), dpi=150)
+    x_org = img_org.ravel()
+    x_cor = img_cor.ravel()
 
+    plt.subplot(211)
+    plt.title('Histogram Showing Pixel Intensities And Counts', color='crimson')
+    plt.hist(x=x_org,
+             bins=256,
+             range=[0, 256],
+             color='crimson')
+    plt.ylabel('Number Of Pixels', color='crimson')
+
+    plt.subplot(212)
+    plt.hist(x=x_cor,
+             bins=256,
+             range=[0, 256],
+             color='crimson')
+    plt.ylabel('Number Of Pixels', color='crimson')
+    plt.xlabel('Pixel Intensity', color='crimson')
+    plt.show()
+
+
+def call_inrrange():
     hsv_min = np.array([lowh, lows, lowv])
     hsv_max = np.array([hih, his, hiv])
     thresh = cv2.inRange(img_hsv, hsv_min, hsv_max)
@@ -78,8 +98,34 @@ def on_hsv_hiv(val):
 
 
 if __name__ == '__main__':
-    img_original = cv2.imread('img\\L1030.jpg', cv2.IMREAD_COLOR)
+
+    enable_correction = False
+
+    img_original = cv2.imread('img\\L1036.jpg', cv2.IMREAD_COLOR)
+    # img_original = cv2.imread('img\\ubuntu.jpg', cv2.IMREAD_COLOR)
+    # img_original = cv2.imread('img\\pepsi.jpg', cv2.IMREAD_COLOR)
+
+    img_original_small = cv2.resize(img_original, (640, 400))
+
+    cv2.namedWindow('Original image')
+    cv2.moveWindow('Original image', 10, 10)
+    cv2.imshow('Original image', img_original_small)
+
+    if enable_correction:
+        hih = 21
+        img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2YCrCb)
+        img_original[:, :, 0] = cv2.equalizeHist(img_original[:, :, 0])
+        img_corrected = cv2.cvtColor(img_original, cv2.COLOR_YCrCb2BGR)
+        img_corrected_small = cv2.resize(img_corrected, (640, 400))
+        cv2.namedWindow('Corrected image')
+        cv2.moveWindow('Corrected image', 10, 10 + 400 + 25)
+        cv2.imshow('Corrected image', img_corrected_small)
+        draw_histogram(img_original, img_corrected)
+        img_original = img_corrected
+
     img_hsv = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
+    img_hsv[:, :, 2] = cv2.equalizeHist(img_hsv[:, :, 2])
+    img_hsv = cv2.fastNlMeansDenoisingColored(img_hsv, None, 10, 10, 7, 21)
 
     cv2.namedWindow(title_window)
     cv2.createTrackbar('Low H', title_window, 0, 255, on_hsv_lowh)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 
 
@@ -23,29 +22,13 @@ def draw_histogram(img):
     plt.show()
 
 
-def draw_contour(img_org, img_thresh):
-    contours, hierarchy = cv2.findContours(image=img_thresh,
-                                           mode=cv2.RETR_TREE,
-                                           method=cv2.CHAIN_APPROX_NONE)
-    # Draw contours on the original image
-    img_contour = img_org.copy()
-    cv2.drawContours(image=img_contour,
-                     contours=contours,
-                     contourIdx=-1,
-                     color=(0, 255, 0),
-                     thickness=2,
-                     lineType=cv2.LINE_AA)
-    cv2.imshow('Contour image', img_contour)
-    cv2.imwrite('img\\out\\img_contour.jpg', img_contour)
-
-
 if __name__ == '__main__':
 
     # Get installed modules version info
     print_modules_version()
 
     # Load an original imag
-    img_original = cv2.imread('img\\L1030.jpg', cv2.IMREAD_COLOR)
+    img_original = cv2.imread('img\\L1035.jpg', cv2.IMREAD_COLOR)
 
     # Convert imag from BGR to the new format
     img_converted = cv2.cvtColor(img_original, cv2.COLOR_BGR2YCrCb)
@@ -57,52 +40,8 @@ if __name__ == '__main__':
     cv2.imshow('Original image', img_original)
     cv2.imshow('Equalized image', img_equalized)
 
-    #cv2.waitKey()
-    #cv2.destroyAllWindows()
-
-    img_gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
-
-    # Threshold parameters
-    thresh = 155    # Threshold value
-    maxval = 255    # Maximum value
-    blockSize = 11  # Size of a pixel neighborhood that is used to calculate a threshold value for the pixel
-    C = 2           # Constant subtracted from the mean or weighted mean
-
-    ret, img_thresh_bunary = cv2.threshold(img_gray, thresh, maxval, cv2.THRESH_BINARY)
-    img_thresh_adaptive_mean = cv2.adaptiveThreshold(img_gray, maxval, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                                     cv2.THRESH_BINARY, blockSize, C)
-    img_thresh_adaptive_gaussian = cv2.adaptiveThreshold(img_gray, maxval, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                                         cv2.THRESH_BINARY, blockSize, C)
-
-    cv2.imshow('Gray image', img_gray)
-    cv2.imshow('Adaptive mean image', img_thresh_adaptive_mean)
-    cv2.imshow('Adaptive gaussian image', img_thresh_adaptive_gaussian)
-
-    # Detect the contours on the binary image using cv22.CHAIN_APPROX_NONE
-    draw_contour(img_original, img_thresh_bunary)
-
-    #contours, hierarchy = cv2.findContours(image=img_thresh_bunary,
-    #                                       mode=cv2.RETR_TREE,
-    #                                       method=cv2.CHAIN_APPROX_NONE)
-    # Draw contours on the original image
-    #img_contour = img_original.copy()
-    #cv2.drawContours(image=img_contour,
-    #                 contours=contours,
-    #                 contourIdx=-1,
-    #                 color=(0, 255, 0),
-    #                 thickness=2,
-    #                 lineType=cv2.LINE_AA)
-    #cv2.imshow('Contour image', img_contour)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    cv2.imwrite('img\\out\\img_equalized.jpg', img_equalized)
-    cv2.imwrite('img\\out\\image_gray.jpg', img_gray)
-    cv2.imwrite('img\\out\\adaptive_mean_image.jpg', img_thresh_adaptive_mean)
-    cv2.imwrite('img\\out\\img_thresh_adaptive_gaussian.jpg', img_thresh_adaptive_gaussian)
-
     draw_histogram(img_original)
+    draw_histogram(img_equalized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
